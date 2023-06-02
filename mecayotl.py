3787304da7ddbfcb63b44e6cc0dae03c987b65b2
olivares-j <avijer@gmail.com>
2023-06-02 18:54:02 +0200

    number of components must be integer

--- mecayotl.py
+++ mecayotl.py
@@ -541,7 +541,7 @@ class Mecayotl(object):
 		aics = np.array(aics)
 		bics = np.array(bics)
 		nmin = np.array(nmin)
-		ngcs = np.array(ngcs)
+		ngcs = np.array(ngcs,dtype=int8)
 		#---------------------
 
 		#------ Find best ---------------------------------
